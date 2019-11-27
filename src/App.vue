80977152c307d255281f807cc5424ddd94164065
HYscript-01 <aqi_hot_01@hotmail.com>
2019-11-27 11:05:10 +0800

    做完插槽

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-view/>
+    <FooterGuide/>
+  </div>
+</template>
+<script>
+// 引入底部组件
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  name: 'App',
+  // 注册组件
+  components: {
+    FooterGuide
+  }
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus"></style>
